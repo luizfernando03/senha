038644da0b9f3e37e489428fca7e21e1362511f1
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-16 13:56:35 -0300

    Atualizando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Usuario usuario = new Usuario();
+
+        System.out.println(Usuario.alterarSenha("Senha001"));
+        System.out.println(Usuario.alterarSenha("Senha002"));
+        System.out.println(Usuario.alterarSenha("Senha003"));
+        System.out.println(Usuario.alterarSenha("Senha004"));
 
     }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Usuario {
+    private static List<String> senhas;
+    private static Scanner scanner;
+
+    public Usuario() {
+        this.senhas = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+
+    }
+
+    public static String alterarSenha(String novaSenha) {
+        System.out.println("Digite a sua senha");
+        String senhaAtual = scanner.nextLine();
+
+        if (senhas.isEmpty() || senhaAtual.equals(senhas.get(senhas.size() - 1))) {
+            if (senhas.size() >= 3 && (novaSenha.equals(senhas.get(senhas.size() - 2)) || novaSenha.equals(senhas.get(senhas.size() - 3)))) {
+                return "A nova senha não pode ser igual a das ultimas três senhas!";
+            } else if (novaSenha.equals(senhaAtual)) {
+                return "A nova senha não pode ser igual a da senha atual";
+
+            } else {
+                senhas.add(novaSenha);
+                return "Senha alterada com sucesso!";
+            }
+        } else {
+            return "Senha atual invalida!";
+        }
+    }
+
 }
